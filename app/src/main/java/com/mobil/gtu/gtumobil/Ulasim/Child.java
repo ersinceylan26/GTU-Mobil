ce60617712caa67b6ec7ff17e1925c14cefe40e1
eceylan26 <ecyln26@gmail.com>
2018-04-16 17:41:28 +0300

    ulasim child halledildi

---
+++ app/src/main/java/com/mobil/gtu/gtumobil/Ulasim/Child.java
@@ -0,0 +1,18 @@
+package com.mobil.gtu.gtumobil.Ulasim;
+
+public class Child
+{
+    String name;
+
+    public Child(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
