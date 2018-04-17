9737d2ad7084bd4cd8fe2a3f9ae07d676e35402e
eceylan26 <ecyln26@gmail.com>
2018-04-17 20:48:16 +0300

    rehber tamamlandı

--- app/src/main/java/com/mobil/gtu/gtumobil/Rehber/RehberActivity.java
+++ app/src/main/java/com/mobil/gtu/gtumobil/Rehber/RehberActivity.java
@@ -11,7 +11,10 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.mobil.gtu.gtumobil.R;
 
@@ -22,6 +25,10 @@ public class RehberActivity extends AppCompatActivity
 {
     RecyclerView myRecycler;
     List<Contact> lstContact;
+    List<Contact> lstContactSearch;
+    Button rehberSearch;
+    EditText rehberAramaText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +36,51 @@ public class RehberActivity extends AppCompatActivity
         setContentView(R.layout.activity_rehber);
 
         lstContact = new ArrayList<>();
+        lstContactSearch = new ArrayList<>();
 
-        lstContact.add(new Contact("Yusuf Sinan Akgül","Öğretim Üyesi","0(212)333 22 11"));
-        lstContact.add(new Contact("Mehmet Göktürk","Öğretim Üyesi","0(212)333 22 11"));
-        lstContact.add(new Contact("Hasari Çelebi","Öğretim Üyesi","0(212)333 22 11"));
-        lstContact.add(new Contact("Meral Şahin","Bil. Müh. Sekreterlik","0(212)333 22 11"));
+        lstContact.add(new Contact("Yusuf Sinan Akgül","Öğretim Üyesi","0( 212)333 22 11"));
+        lstContact.add(new Contact("Mehmet Göktürk","Öğretim Üyesi","0 (212)333 22 11"));
+        lstContact.add(new Contact("Hasari Çelebi","Öğretim Üyesi","0 (212)333 22 11"));
+        lstContact.add(new Contact("Meral Şahin","Bil. Müh. Sekreterlik","0 (212)333 22 11"));
 
         myRecycler = (RecyclerView) findViewById(R.id.rehberRecycler);
         RehberAdapter mAdapter = new RehberAdapter(this,lstContact);
         myRecycler.setLayoutManager(new LinearLayoutManager(this));
         myRecycler.setAdapter(mAdapter);
+
+        rehberSearch = findViewById(R.id.rehberAramaButton);
+        rehberAramaText = findViewById(R.id.rehberAramaTxt);
+
+        rehberSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                lstContactSearch.clear();
+
+                for(int i = 0 ; i < lstContact.size();i++)
+                {
+                    Log.w("1", rehberAramaText.getText().toString());
+                    Log.w("2", lstContact.get(i).getName());
+
+                   if(lstContact.get(i).getName().toUpperCase().contains(rehberAramaText.getText().toString().toUpperCase())
+                           ||lstContact.get(i).getUnvan().toUpperCase().contains(rehberAramaText.getText().toString().toUpperCase()))
+                   {
+                       lstContactSearch.add(lstContact.get(i));
+                       Log.w("4444", lstContactSearch.get(0).getName());
+                   }
+                }
+
+                for(int i = 0 ;i<lstContactSearch.size();i++)
+                {
+                    Log.w("3333", lstContactSearch.get(i).getName());
+                }
+
+                myRecycler = (RecyclerView) findViewById(R.id.rehberRecycler);
+                RehberAdapter mAdapter = new RehberAdapter(getApplicationContext(),lstContactSearch);
+                myRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                myRecycler.setAdapter(mAdapter);
+            }
+        });
 
 
 
